7d0906cf822e0d6f9a6d8412b15f12ed1d4acf9a
ncdulo <nick.cardullo@gmail.com>
2019-11-28 17:04:51 -0500

    Changed window title

--- loader_functions/initialize_new_game.py
+++ loader_functions/initialize_new_game.py
@@ -15,7 +15,7 @@ from map_objects.game_map import GameMap
 from render_functions import RenderOrder
 
 def get_constants():
-    window_title = 'Rougelike Tutorial Revised'
+    window_title = 'Baby\'s First Rougelike'
 
     screen_width = 80
     screen_height = 50
