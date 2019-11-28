f4b1d4b434df04318370a4f2c48573709e4201f5
ncdulo <nick.cardullo@gmail.com>
2019-11-28 17:01:47 -0500

    Part 11 - finished level up system, added character stat screen

--- entity.py
+++ entity.py
@@ -10,7 +10,7 @@ class Entity:
     A generic object to represent players, enemies, items, etc.
     """
     def __init__(self, x, y, char, color, name, blocks=False, render_order=RenderOrder.CORPSE, fighter=None, ai=None,
-                 item=None, inventory=None, stairs=None):
+                 item=None, inventory=None, stairs=None, level=None):
         self.x = x
         self.y = y
         self.char = char
@@ -23,6 +23,7 @@ class Entity:
         self.item = item
         self.inventory = inventory
         self.stairs = stairs
+        self.level = level
 
         if self.fighter:
             self.fighter.owner = self
@@ -38,6 +39,9 @@ class Entity:
 
         if self.stairs:
             self.stairs.owner = self
+
+        if self.level:
+            self.level.owner = self
 
     def move(self, dx, dy):
         # Move the entity by a given amount
